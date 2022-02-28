e8584da0467da27a59a44c0e598e3f25c3b75c61
mkmanmeet19 <mkmanmeet19@gmail.com>
2022-02-28 23:54:57 +0530

    commit 3

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -4,6 +4,8 @@ import Home from './Pages/Home/Home';
 import Auth from './Pages/Auth/Auth';
 import Questions from './Pages/Questions/Questions';
 import AskQuestion from './Pages/AskQuestion/AskQueston';
+import DisplayQuestions from './Pages/Questions/DisplayQuestions';
+
 
 const AllRoutes = () => {
   return (
@@ -13,6 +15,7 @@ const AllRoutes = () => {
         <Route path='/Auth' element={<Auth/>} />
         <Route path='/Questions' element={<Questions/>} />
         <Route path='/AskQuestion' element={<AskQuestion/>} />
+        <Route path='/Questions/:id' element={<DisplayQuestions />} />
       </Routes>
     </div>);
 };
